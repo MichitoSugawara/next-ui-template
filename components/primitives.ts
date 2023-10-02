389ade5192f82dd939921541b06601fcf5760a27
Michito Sugawara <michitogawara@gmail.com>
2023-10-03 08:03:13 +0900

    Initial commit

--- components/primitives.ts
+++ components/primitives.ts
@@ -1,53 +1,45 @@
-import { tv } from "tailwind-variants";
+import { tv } from 'tailwind-variants'
 
 export const title = tv({
-	base: "tracking-tight inline font-semibold",
-	variants: {
-		color: {
-			violet: "from-[#FF1CF7] to-[#b249f8]",
-			yellow: "from-[#FF705B] to-[#FFB457]",
-			blue: "from-[#5EA2EF] to-[#0072F5]",
-			cyan: "from-[#00b7fa] to-[#01cfea]",
-			green: "from-[#6FEE8D] to-[#17c964]",
-			pink: "from-[#FF72E1] to-[#F54C7A]",
-			foreground: "dark:from-[#FFFFFF] dark:to-[#4B4B4B]",
-		},
-		size: {
-			sm: "text-3xl lg:text-4xl",
-			md: "text-[2.3rem] lg:text-5xl leading-9",
-			lg: "text-4xl lg:text-6xl",
-		},
-		fullWidth: {
-			true: "w-full block",
-		},
-	},
-	defaultVariants: {
-		size: "md",
-	},
-	compoundVariants: [
-		{
-			color: [
-				"violet",
-				"yellow",
-				"blue",
-				"cyan",
-				"green",
-				"pink",
-				"foreground",
-			],
-			class: "bg-clip-text text-transparent bg-gradient-to-b",
-		},
-	],
-});
+  base: 'inline font-semibold tracking-tight',
+  compoundVariants: [
+    {
+      class: 'bg-gradient-to-b bg-clip-text text-transparent',
+      color: ['violet', 'yellow', 'blue', 'cyan', 'green', 'pink', 'foreground'],
+    },
+  ],
+  defaultVariants: {
+    size: 'md',
+  },
+  variants: {
+    color: {
+      blue: 'from-[#5EA2EF] to-[#0072F5]',
+      cyan: 'from-[#00b7fa] to-[#01cfea]',
+      foreground: 'dark:from-[#FFFFFF] dark:to-[#4B4B4B]',
+      green: 'from-[#6FEE8D] to-[#17c964]',
+      pink: 'from-[#FF72E1] to-[#F54C7A]',
+      violet: 'from-[#FF1CF7] to-[#b249f8]',
+      yellow: 'from-[#FF705B] to-[#FFB457]',
+    },
+    fullWidth: {
+      true: 'block w-full',
+    },
+    size: {
+      lg: 'text-4xl lg:text-6xl',
+      md: 'text-[2.3rem] leading-9 lg:text-5xl',
+      sm: 'text-3xl lg:text-4xl',
+    },
+  },
+})
 
 export const subtitle = tv({
-	base: "w-full md:w-1/2 my-2 text-lg lg:text-xl text-default-600 block max-w-full",
-	variants: {
-		fullWidth: {
-			true: "!w-full",
-		},
-	},
-  defaultVariants:{
-    fullWidth: true
-  }
-});
+  base: 'my-2 block w-full max-w-full text-lg text-default-600 md:w-1/2 lg:text-xl',
+  defaultVariants: {
+    fullWidth: true,
+  },
+  variants: {
+    fullWidth: {
+      true: '!w-full',
+    },
+  },
+})
